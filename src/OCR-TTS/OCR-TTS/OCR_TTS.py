4be9209aa8cd385a684efccabbc5017c7108d092
psy124 <psy12345663@gmail.com>
2022-05-22 11:34:30 +0900

    gtts코드 추가 및 ocr과 결합

--- src/OCR-TTS/OCR-TTS/OCR_TTS.py
+++ src/OCR-TTS/OCR-TTS/OCR_TTS.py
@@ -1,12 +1,20 @@
 from PIL import Image
 import pytesseract
+from gtts import gTTS
 
 pytesseract.pytesseract.tesseract_cmd = r'C:\Program Files\Tesseract-OCR\tesseract.exe'
-text = pytesseract.image_to_string(Image.open("C:\\Users\\USER\\source\\repos\\OCR-TTS\\n1.PNG"), lang="kor")
-print(text)
-print(text.replace(" ",""))
+ocr_text = pytesseract.image_to_string(Image.open("n1.PNG"), lang="kor")
+print(ocr_text.replace(" ",""))
 
 with open("news.txt","w",encoding="utf8") as f:
-    f.write(text)
     f.write("\n\n\n")
-    f.write(text.replace(" ",""))
+    f.write(ocr_text.replace(" ",""))
+
+#txt파일 mp3파일로 변환
+conversion = 'news.txt'
+
+with open(conversion, mode = 'r', encoding='UTF-8') as text:
+    script = text.read()
+
+speech = gTTS(text = script, lang='ko')
+speech.save('read_news.mp3')
