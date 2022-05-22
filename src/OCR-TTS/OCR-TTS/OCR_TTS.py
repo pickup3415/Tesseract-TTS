b1298d8c91349a82f663f05a57fcd6959c95be8e
ghida5130 <ghida5130@gmail.com>
2022-05-22 15:10:12 +0900

    recognition rate improvement using opencv

--- src/OCR-TTS/OCR-TTS/OCR_TTS.py
+++ src/OCR-TTS/OCR-TTS/OCR_TTS.py
@@ -3,8 +3,12 @@ import pytesseract
 from gtts import gTTS
 import cv2
 
+image = cv2.imread('n1.PNG')
+gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+gray = cv2.threshold(gray, 0, 255, cv2.THRESH_BINARY | cv2.THRESH_OTSU)
+
 pytesseract.pytesseract.tesseract_cmd = r'C:\Program Files\Tesseract-OCR\tesseract.exe'
-ocr_text = pytesseract.image_to_string(Image.open("n1.PNG"), lang="kor")
+ocr_text = pytesseract.image_to_string(image, lang="kor")
 print(ocr_text.replace(" ",""))
 
 with open("news.txt","w",encoding="utf8") as f:
