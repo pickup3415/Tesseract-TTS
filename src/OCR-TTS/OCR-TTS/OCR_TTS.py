e39d25b4d9f502d3c6abfae56799ef5cae2e724d
ghida5130 <ghida5130@gmail.com>
2022-05-25 14:57:27 +0900

    "Error correction & added language select function"

--- src/OCR-TTS/OCR-TTS/OCR_TTS.py
+++ src/OCR-TTS/OCR-TTS/OCR_TTS.py
@@ -2,13 +2,27 @@ from PIL import Image
 import pytesseract
 from gtts import gTTS
 import cv2
+from typing import Text
+import sys
+
+a = int(input("English = 1, Korean = 2 : ")
 
 image = cv2.imread('n1.PNG')
 gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
 gray = cv2.threshold(gray, 0, 255, cv2.THRESH_BINARY | cv2.THRESH_OTSU)
 
+if a==1:
+    lan="eng"
+    lan2="en"
+elif a==2:
+    lan="kor"
+    lan2="ko"
+else:
+    print("Error")
+    sys.exit()
+
 pytesseract.pytesseract.tesseract_cmd = r'C:\Program Files\Tesseract-OCR\tesseract.exe'
-ocr_text = pytesseract.image_to_string(image, lang="kor")
+ocr_text = pytesseract.image_to_string(image, lang=lan)
 print(ocr_text.replace(" ",""))
 
 with open("news.txt","w",encoding="utf8") as f:
@@ -21,5 +35,5 @@ conversion = 'news.txt'
 with open(conversion, mode = 'r', encoding='UTF-8') as text:
     script = text.read()
 
-speech = gTTS(text = script, lang='ko')
+speech = gTTS(text = script, lang=lan2)
 speech.save('read_news.mp3')
